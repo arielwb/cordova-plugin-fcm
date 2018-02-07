90a9ba1ea1541546dbe938badc6d1725a8c309d3
ariel.borges <ariel.borges@cwi.com.br>
2018-02-07 18:19:23 -0200

    add notification type verification

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -35,27 +35,33 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
         Log.d(TAG, "==> MyFirebaseMessagingService onMessageReceived");
-		
-		if( remoteMessage.getNotification() != null){
-			Log.d(TAG, "\tNotification Title: " + remoteMessage.getNotification().getTitle());
-			Log.d(TAG, "\tNotification Message: " + remoteMessage.getNotification().getBody());
-		}
-		
-		Map<String, Object> data = new HashMap<String, Object>();
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "\tNotification Title: " + remoteMessage.getNotification().getTitle());
+            Log.d(TAG, "\tNotification Message: " + remoteMessage.getNotification().getBody());
+        }
+
+        Map<String, Object> data = new HashMap<String, Object>();
         data.put("wasTapped", false);
         data.put("received", new Date().getTime());
-		for (String key : remoteMessage.getData().keySet()) {
-                Object value = remoteMessage.getData().get(key);
-                Log.d(TAG, "\tKey: " + key + " Value: " + value);
-				data.put(key, value);
+        for (String key : remoteMessage.getData().keySet()) {
+            Object value = remoteMessage.getData().get(key);
+            Log.d(TAG, "\tKey: " + key + " Value: " + value);
+            data.put(key, value);
         }
-		
-		Log.d(TAG, "\tNotification Data: " + data.toString());
-        FCMPlugin.sendPushPayload( data, getSharedPreferences(FCMPlugin.pushStorageKey, Context.MODE_PRIVATE));
+
+        Log.d(TAG, "\tNotification Data: " + data.toString());
+        if (!data.get("tipo").equals("1")) {
+            FCMPlugin.sendPushPayload(data, getSharedPreferences(FCMPlugin.pushStorageKey, Context.MODE_PRIVATE));
+        }
         FCMPlugin.sendPushConfirmation(this.getApplicationContext(), "1");
 
-        String displayTitle = data.get(FCMPlugin.pushDataTitleKey) != null ? data.get(FCMPlugin.pushDataTitleKey).toString() : "Turistur"; 
-        String displayBody = data.get(FCMPlugin.pushDataBodyKey) != null ? data.get(FCMPlugin.pushDataBodyKey).toString() : "Você tem uma nova notificação da Turistur. Abra o app e confira!"; 
+        String displayTitle = data.get(FCMPlugin.pushDataTitleKey) != null
+                ? data.get(FCMPlugin.pushDataTitleKey).toString()
+                : "Turistur";
+        String displayBody = data.get(FCMPlugin.pushDataBodyKey) != null
+                ? data.get(FCMPlugin.pushDataBodyKey).toString()
+                : "Você tem uma nova notificação da Turistur. Abra o app e confira!"; 
 
         sendNotification(displayTitle, displayBody, data);
     }
@@ -69,24 +75,20 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private void sendNotification(String title, String messageBody, Map<String, Object> data) {
         Intent intent = new Intent(this, FCMPluginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		for (String key : data.keySet()) {
-			intent.putExtra(key, data.get(key).toString());
-		}
+        for (String key : data.keySet()) {
+            intent.putExtra(key, data.get(key).toString());
+        }
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-            .setSmallIcon(getApplicationContext().getResources().getIdentifier(FCMPlugin.pushIconName,"drawable", getApplicationContext().getPackageName()))
-            .setContentTitle(title)
-            .setContentText(messageBody)
-            .setAutoCancel(true)
-            .setSound(defaultSoundUri)
-            .setColor(Color.parseColor(FCMPlugin.pushIconColor))
-            .setContentIntent(pendingIntent);
+                .setSmallIcon(getApplicationContext().getResources().getIdentifier(FCMPlugin.pushIconName, "drawable",
+                        getApplicationContext().getPackageName()))
+                .setContentTitle(title).setContentText(messageBody).setAutoCancel(true).setSound(defaultSoundUri)
+                .setColor(Color.parseColor(FCMPlugin.pushIconColor)).setContentIntent(pendingIntent);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
